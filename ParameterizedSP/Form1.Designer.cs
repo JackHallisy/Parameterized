492d730d622384d1cbffb07b9c963106efdeab1b
JackHallisy <39491444+JackHallisy@users.noreply.github.com>
2018-10-18 08:50:44 -0400

    Add files via upload

---
+++ ParameterizedSP/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ParameterizedSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CustomerIdTextBox = new System.Windows.Forms.TextBox();
+            this.GetTotalSalesButton = new System.Windows.Forms.Button();
+            this.TotalSalesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Id:";
+            // 
+            // CustomerIdTextBox
+            // 
+            this.CustomerIdTextBox.Location = new System.Drawing.Point(146, 71);
+            this.CustomerIdTextBox.Name = "CustomerIdTextBox";
+            this.CustomerIdTextBox.Size = new System.Drawing.Size(137, 22);
+            this.CustomerIdTextBox.TabIndex = 1;
+            // 
+            // GetTotalSalesButton
+            // 
+            this.GetTotalSalesButton.Location = new System.Drawing.Point(56, 131);
+            this.GetTotalSalesButton.Name = "GetTotalSalesButton";
+            this.GetTotalSalesButton.Size = new System.Drawing.Size(150, 45);
+            this.GetTotalSalesButton.TabIndex = 2;
+            this.GetTotalSalesButton.Text = "Get Total Sales";
+            this.GetTotalSalesButton.UseVisualStyleBackColor = true;
+            this.GetTotalSalesButton.Click += new System.EventHandler(this.GetTotalSalesButton_Click);
+            // 
+            // TotalSalesLabel
+            // 
+            this.TotalSalesLabel.AutoSize = true;
+            this.TotalSalesLabel.Location = new System.Drawing.Point(63, 209);
+            this.TotalSalesLabel.Name = "TotalSalesLabel";
+            this.TotalSalesLabel.Size = new System.Drawing.Size(83, 17);
+            this.TotalSalesLabel.TabIndex = 3;
+            this.TotalSalesLabel.Text = "Total Sales:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TotalSalesLabel);
+            this.Controls.Add(this.GetTotalSalesButton);
+            this.Controls.Add(this.CustomerIdTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CustomerIdTextBox;
+        private System.Windows.Forms.Button GetTotalSalesButton;
+        private System.Windows.Forms.Label TotalSalesLabel;
+    }
+}
+
---
+++ ParameterizedSP/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[GetCustomerSales]
+(
+@CustomerId char(5),
+@TotalSales money OUTPUT
+)
+
+AS
+	SELECT @TotalSales = SUM(Quantity * UnitPrice)
+	FROM (Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID)
+	INNER JOIN [Order Details]
+	ON Orders.OrderID = [Order Details].OrderID
+	WHERE Customers.CustomerID = @CustomerId
+
+RETURN 
